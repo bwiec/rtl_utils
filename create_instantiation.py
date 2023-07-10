15bb7b1a8051bf3fe7c5019750e321ea48e584ff
Brian Wiec <bswiec@gmail.com>
2023-07-10 10:37:36 -0400

    Added debug flag

--- create_instantiation.py
+++ create_instantiation.py
@@ -7,6 +7,7 @@ from module_parser import module_parser
 # Entry point of this file
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog=sys.argv[0], description='Create instantiation template from module/entity definition')
+    parser.add_argument('--debug', action='store_true', help='Print script debug messages')
     parser.add_argument('--inc_sig_property_comments', action='store_true', help='Adds comments about port type and dimensionality to each signal instantiation')
     parser.add_argument('filename',nargs='+')
     Args = sys.argv
@@ -15,4 +16,5 @@ if __name__ == "__main__":
 
     for filename in args.filename:
         parser = module_parser(filename)
-        print(parser.get_module_properties())
+        if args.debug:
+            print(parser.get_module_properties())
